e0528006d2d66ff3b195f0a93bbd7aebbc2d7b7c
Gabriel C. Brandão <biel.brandao2004@gmail.com>
2022-10-05 00:01:31 -0300

    WIP

--- web2/src/lib/apollo.ts
+++
@@ -1,6 +0,0 @@
-import {ApolloClient, InMemoryCache} from '@apollo/client';
-
-export const apolloClient = new ApolloClient({
-  cache: new InMemoryCache(),
-  uri: "https://api-sa-east-1.hygraph.com/v2/cl8ul4tfv5von01uq7r88ahum/master"
-});
