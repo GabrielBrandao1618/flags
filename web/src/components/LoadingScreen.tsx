3cb82d8e9b91baecc7fc36667fe979c37aa86504
Gabriel C. Brandão <biel.brandao2004@gmail.com>
2022-10-16 20:23:58 -0300

    Pequeno ajuste em cores

--- web/src/components/LoadingScreen.tsx
+++ web/src/components/LoadingScreen.tsx
@@ -1,6 +1,6 @@
 export function LoadingScreen(){
   return (
-    <div className="h-screen w-screen bg-gray-800 flex justify-center items-center">
+    <div className="h-screen w-screen bg-gray-900 flex justify-center items-center">
       <h1 className="text-white font-bold text-3xl">
         Loading...
       </h1>
